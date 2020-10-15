27e495559d8195e237f7742888d1ca40324df539
voiciphil <voiciphil@gmail.com>
2020-10-15 19:42:23 +0900

    Print the total in January

--- chapter2-3/src/main/java/BankTransactionAnalyzerSimple.java
+++ chapter2-3/src/main/java/BankTransactionAnalyzerSimple.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -19,5 +22,17 @@ public class BankTransactionAnalyzerSimple {
             total += amount;
         }
         System.out.println("The total for all transactions is " + total);
+
+        total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+        }
+        System.out.println("The total for all transactions in January is " + total);
     }
 }
